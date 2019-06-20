2b1c0603100f9b93fe42a3bc805021091813c49c
Alexandre Bangma <alexandre.bangma@student-cs.fr>
2019-06-20 11:51:42 +0200

    ajout note sur filmnotes

--- server/src/repositories/FilmNotes.py
+++ server/src/repositories/FilmNotes.py
@@ -10,4 +10,4 @@ class FilmNotesRepository:
     """The repository for the notation average truc"""
     @staticmethod
     def get(user_first_name,user_last_name):
-        return db.session.query(Notation.movie_title).filter_by(user_first_name=user_first_name).filter_by(user_last_name=user_last_name).all()
+        return db.session.query(Notation.movie_title, Notation.note).filter_by(user_first_name=user_first_name).filter_by(user_last_name=user_last_name).all()
