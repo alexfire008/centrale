808cef11cca5be7a3f41f3c793adc77dac12cba7
Alexandre Bangma <alexandre.bangma@student-cs.fr>
2019-06-20 11:55:47 +0200

    prediction le retour

--- server/src/routes/__init__.py
+++ server/src/routes/__init__.py
@@ -4,3 +4,4 @@ from .notation import NOTATION_BLUEPRINT
 from .notationAverageFilm import AVR_FILM_NOTATION_BLUEPRINT
 from .FilmTop5 import FILM_TOP_NOTATION_BLUEPRINT
 from .FilmNotes import FILMNOTES_BLUEPRINT
+from .Prediction import PREDICTION_BLUEPRINT
